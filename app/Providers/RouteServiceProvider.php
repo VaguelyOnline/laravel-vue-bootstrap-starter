681de1e94a48001d7f41f5bd7071d0ec6aeab9a9
Nathan <nathan@calibrae.com>
2023-11-06 14:55:31 +0000

    Fixed welcome page display issue

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/example';
+    public const HOME = '/example1';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
